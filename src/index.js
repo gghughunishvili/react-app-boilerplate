151e9bcbcdc7957001c32c150a06563fde06d4b6
Giorgi Ghughunishvili <giorgi855007@gmail.com>
2017-08-20 22:03:28 +0400

    added react router implementation

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/App'
+import About from './components/About'
+import Contact from './components/Contact'
+import SignIn from './components/SignIn'
+import SignUp from './components/SignUp'
+import { Router, browserHistory } from 'react-router'
+import './assets/global.scss';
 
-const App = () => {
-    return (<div>Hello World!</div>)
+const routes = {
+    path: '/',
+    indexRoute: { component: App },
+    childRoutes: [
+        { path: 'about', component: About },
+        { path: 'contact', component: Contact },
+        { path: 'login', component: SignIn },
+        { path: 'register', component: SignUp },
+    ]
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Router history={browserHistory} routes={routes} />, document.getElementById('root'))
