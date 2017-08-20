245ccc30fb6fef1fa1a462af0464950c7dc64865
Giorgi Ghughunishvili <giorgi855007@gmail.com>
2017-08-20 22:02:59 +0400

    added bunch of packages to run from webpack and optimize css, js and so on... For react browserHistory use, added historyApiFallback: true in order to serve any route call from index.html and then react router will know what to do

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+var OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: __dirname + "dist/assets",
+        filename: "bundle.js",
+        publicPath: "assets"
+    },
+    devServer: {
+        inline: true,
+        contentBase: "./dist",
+        port: 3000,
+        historyApiFallback: true
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                loaders: ["babel-loader?presets[]=es2015,presets[]=stage-0,presets[]=latest,presets[]=react"]
+            },
+            {
+                test: /\.json$/,
+                exclude: /(node_modules)/,
+                loader: 'json-loader'
+            },
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader!autoprefixer-loader'
+
+            },
+            {
+                test: /\.scss/,
+                loader: 'style-loader!css-loader!autoprefixer-loader!sass-loader'
+            }
+        ]
+    },
+    plugins: [
+        new OptimizeCssAssetsPlugin({
+            assetNameRegExp: /\.optimize\.css$/g,
+            cssProcessor: require('cssnano'),
+            cssProcessorOptions: {discardComments: {removeAll: true}},
+            canPrint: true
+        })
+    ]
+}
